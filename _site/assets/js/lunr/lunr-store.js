e65ec1bb9e1bf78cd80dae5ac14f1606fb323d47
hon99oo <kanghonggoo@naver.com>
2022-03-04 01:52:20 +0900

    update: de01

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -252,8 +252,8 @@ var store = [{
         "teaser": null
       },{
         "title": "DataEngineer가 되기 위한 기술 스택 정리",
-        "excerpt":"Data Development      Crawler   ETL   Docker   k8s   airflow      Data Platform     Data Orchestration   Data Storage            Data Lake       Data Warehouse           Data Catalog   Data Monitoring            Data Validation       Data Notification              Data Governance         하둡 스파크 카프카 스칼라…? 알아야할게 산더미군..!  ","categories": ["Docker"],
-        "tags": ["Docker"],
-        "url": "/docker/de_01/",
+        "excerpt":"Data Development      Crawler   ETL   Docker   k8s   airflow      Data Platform     Data Orchestration   Data Storage            Data Lake       Data Warehouse           Data Catalog   Data Monitoring            Data Validation       Data Notification              Data Governance         하둡 스파크 카프카 스칼라…? 알아야할게 산더미군..!  ","categories": ["DataEngineering"],
+        "tags": ["DataEngineering"],
+        "url": "/dataengineering/de_01/",
         "teaser": null
       }]
