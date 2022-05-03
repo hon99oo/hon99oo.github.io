dffbc120ad85a5bbc4bf0668fb1de66c699b5288
hon99oo <cocoti5797@gmail.com>
2022-05-03 23:57:57 +0900

    add csapp

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -341,12 +341,6 @@ var store = [{
         "url": "/csapp/csapp_22/",
         "teaser": null
       },{
-        "title": "[CS:APP] Chapter 07 링커",
-        "excerpt":" ","categories": ["CSAPP"],
-        "tags": ["CS:APP","CS"],
-        "url": "/csapp/csapp_23/",
-        "teaser": null
-      },{
         "title": "[프로그래머스/Python] Level1 체육복 (그리디)",
         "excerpt":"프로그래머스(Programmers) 코딩테스트 연습   Level1 그리디 체육복 파이썬(Python) 풀이      풀이로 이동!   문제풀이     그리디 문제는 ‘현재 상황에서 지금 당장 좋은 것만 고르는 방법’을 의미한다. 알고리즘의 패턴보다는 풀이하는 사람의 아이디어가 더 중요한 알고리즘이다. 우선 체육복 문제는 인접한 index의 학생들만 여벌의 옷을 받을 수 있으므로 인접한 index에 대해서만 생각해주면 된다. 또한 도난당한 학생이 여벌의 옷을 가지고 올 수 있기 때문에 이 부분에서 예외처리를 해주어야한다.       lost와 reserve를 set형식으로 변경해준뒤 각각의 차집합을 구해준다.(도난당한 학생과 여벌옷을 가져온 학생이 중복된 경우를 제거하기 위해)   reserve를 기준으로 인접한 lost를 조회하여 존재하면 reserve에서 해당 value를 삭제해주고 count(옷을 빌린 학생 수)를 1 올려준다.   전체에서 lost에 남아있는 값들의 길이를 빼주고 count(옷을 빌린 학생 수)를 더해준다.   위에 값을 answer로 return한다.   코드       def solution(n, lost, reserve):         count = 0         lost, reserve = list(set(lost)-set(reserve)), list(set(reserve)-set(lost))         for lost_v in lost:             for reserve_v in reserve:                 if lost_v - 1 == reserve_v or reserve_v == lost_v + 1:                     reserve.remove(reserve_v)                     count += 1                     break              answer = n - len(lost) + count              return answer          if __name__ == \"__main__\":         n = 5         lost = [1,2,4]         reserve = [2,3,4,5]         print(solution(n,lost,reserve)) ","categories": ["programmers"],
         "tags": ["Algorithm","Python","프로그래머스 코테"],
@@ -429,5 +423,17 @@ var store = [{
         "excerpt":"이코테   이진탐색 나무 자르기(Python)   문제   문제로 이동!   풀이     나무를 자를 수 있는 길이는 0부터 가장 긴 나무의 길이다. 0~가장 긴 나무 길이를 이진 탐색을 하며 잘려진 나무의 합이 목표 길이보다 크다면 자른 길이를 저장한다. 최대값을 구하기 위해서 탐색이 종료될 때까지 조건에 맞는 길이를 찾아간다.       나무 길이의 최대 값을 찾는다.   start는 0, end는 나무 길이의 최대 값으로 설정한다.   중간 값을 찾고, 나무 길이 - 중간 값을 리스트 컴프리헨션으로 음수가 되는 값은 0으로 변경하여 전체 합을 구한다.   전체합과 target을 비교하여 전체합 보다 target이 작거나 같으면 결과에 중간값을 저장한다.   탐색이 끝날 때 까지 반복하며 최대 값을 구한다.   추가 설명     해당 문제는 전형적인 이진 탐색 문제이자, 파라메트릭 서치parametric search 유형의 문제라고 한다. “원하는 조건을 만족하는 가장 알맞은 값을 찾는 문제”에 주로 사용한다. 예를 들어 범위 내에서 조건을 만족하는 가장 큰 값을 찾으라는 최적화 문제라면 이진 탐색으로 결정 문제를 해결하며 범위를 좁혀갈 수 있다.    코드            def solution(array, target):         end = max(array)         return binary_search(array, target, 0, end)               def binary_search(array, target, start, end):         result = 0         while start &lt;= end:             mid = (start + end) // 2             total = sum([0 if i-mid &lt; 0 else i-mid for i in array])             if total &lt; target:                 end = mid - 1             else:                 result = mid                 start = mid + 1              return result          if __name__ == \"__main__\":         target = 6         array = [19, 15, 10, 17]         print(solution(array, target)) ","categories": ["이코테"],
         "tags": ["Algorithm","Python","이코테"],
         "url": "/%EC%9D%B4%EC%BD%94%ED%85%8C/this_10/",
+        "teaser": null
+      },{
+        "title": "[DataEngineering] '스페이스워크'에서의 첫 번째 데이터엔지니어 인턴 회고",
+        "excerpt":" ","categories": ["DataEngineering"],
+        "tags": ["DataEngineering"],
+        "url": "/dataengineering/de_02/",
+        "teaser": null
+      },{
+        "title": "[CS:APP] Chapter 07 링커",
+        "excerpt":" ","categories": ["CSAPP"],
+        "tags": ["CS:APP","CS"],
+        "url": "/csapp/csapp_23/",
         "teaser": null
       }]
